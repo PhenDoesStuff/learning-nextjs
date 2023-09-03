7a47f680e793af06ec9c755d148c07b359cc0bb3
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-09-03 13:06:06 -0400

    Added links to other pages

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,11 @@
+import Link from 'next/link';
+
 const About = () => {
-	return <h1>About</h1>;
+	return (
+		<>
+			<h1>About</h1>
+			<Link href='/'>Link to Home Pag</Link>
+		</>
+	);
 };
 export default About;
