5d5b2f59a13f1a0247b8517fcb72b4b87a6d2e42
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-09-05 21:22:44 -0400

    Created a new page where we will see each user's information. Also creating a reusable component that will take props

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,10 +1,27 @@
+import { Suspense } from 'react';
+import UserPosts from './userPosts';
+
 type Params = {
 	params: {
 		userId: string;
 	};
 };
 
-const UserPage = ({ params: { userId } }: Params) => {
-	return <div>UserPage</div>;
+const UserPage = async ({ params: { userId } }: Params) => {
+	const userData: Promise<User> = getUser(userId);
+	const userPostsData: Promise<Post[]> = getUserPosts(userId);
+	// Above means that we're just sending both of these at the same time and we aren't waiting for anything
+
+	// const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
+	return (
+		<>
+			<h2>{user.name}</h2>
+			<br />
+			<Suspense fallback={<h2>Loading...</h2>}>
+				<UserPosts posts={userPosts} />
+			</Suspense>
+		</>
+	);
 };
 export default UserPage;
---
+++ lib/getUserPosts.ts
@@ -0,0 +1,11 @@
+const getUserPosts = async (userId: string) => {
+	const res = await fetch(
+		`https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+	);
+
+	if (!res.ok) {
+		throw new Error('Failed to fetch user');
+	}
+
+	return res.json();
+};
