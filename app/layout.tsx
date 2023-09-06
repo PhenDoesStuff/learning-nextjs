fdc908433298388608007fa357cd0e8c07f5718b
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-09-05 21:02:59 -0400

    Removed pages that aren't needed for this portion of the course. Created a libs folder that has an API call. Going through the users from that API call and populating them on the users page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>
-				<nav>
-					<h1>My Navbar</h1>
-				</nav>
-				{children}
-			</body>
+			<body className={inter.className}>{children}</body>
 		</html>
 	);
 }
